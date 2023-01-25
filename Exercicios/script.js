7c3eec273fa244e7841623d14097b947332e06b3
jvitor691 <jvitormarques2@hotmail.com>
2023-01-24 23:07:42 -0300

    exercicios feitos

---
+++ Exercicios/script.js
@@ -0,0 +1,37 @@
+/**function BemVindo(){
+    // console.log("Seja Bem-Vindo!!!")
+    // alert("Seja Bem-Vindo!!!")
+}
+BemVindo()**/
+
+// function CalcularIdade(ano){
+//     let idade = 2023 - ano;
+//     // console.log("Idade:" + idade)
+//     alert("Thiago tem " + idade + " anos.")
+// }
+// CalcularIdade(1993);
+
+// function CalcularPercentual(total,qntDeAluno){
+//     let AlunoPresentes = total - qntDeAluno;
+
+//     let percentual = (AlunoPresentes * 100) / total;
+
+//     // return console.log("O percentual de alunos presentes é " + percentual + "%")
+//     return alert("O percentual de alunos presentes é " + percentual + "%")
+
+// }
+
+// CalcularPercentual(20,11)
+
+// function Media(teorica,pratica){
+//     let total = 10;
+
+//     let calcularMedia = ((2 * teorica) + pratica) / total;
+
+//     return console.log("A média do alino é " + calcularMedia + " .")
+// //    return alert("A média do alino é " + calcularMedia + " .")
+
+// }
+
+// Media(7,8)
+
