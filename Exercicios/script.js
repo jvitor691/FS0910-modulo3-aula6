192d9eab13f5202f8cbe8ea2882543b5b25e7a11
jvitor691 <jvitormarques2@hotmail.com>
2023-01-31 23:00:08 -0300

    exercicos incompleto

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -1,37 +1,68 @@
-/**function BemVindo(){
-    // console.log("Seja Bem-Vindo!!!")
-    // alert("Seja Bem-Vindo!!!")
+ function BemVindo(){
+     document.getElementById("tela").innerHTML = "<h2>Seja Bem-Vindo, Thiago!!!</h2><br><p>Você agora pertence a melhor turma da Digital College!!</p>"
 }
-BemVindo()**/
 
-// function CalcularIdade(ano){
-//     let idade = 2023 - ano;
-//     // console.log("Idade:" + idade)
-//     alert("Thiago tem " + idade + " anos.")
-// }
-// CalcularIdade(1993);
+ function CalcularIdade(ano){
+    let idade = 2023 - ano;
 
-// function CalcularPercentual(total,qntDeAluno){
-//     let AlunoPresentes = total - qntDeAluno;
+     document.getElementById("tela").innerHTML = "Thiago tem " + idade + " anos.";
+ }
 
-//     let percentual = (AlunoPresentes * 100) / total;
 
-//     // return console.log("O percentual de alunos presentes é " + percentual + "%")
-//     return alert("O percentual de alunos presentes é " + percentual + "%")
+ function CalcularPercentual(total,qntDeAluno){
+     let AlunoPresentes = total - qntDeAluno;
 
-// }
+     let percentual = ((AlunoPresentes * 100) / total).toFixed(1);
+    
+     if(total == 19){
+     document.getElementById("tela").innerHTML = "O percentual de alunos não presentes é " + percentual + "%. Esse valor é referente a alunos ativos, que seriam "
+      + total + " de alunos."
+     }
 
-// CalcularPercentual(20,11)
+    return document.getElementById("tela").innerHTML =
+        "O percentual de alunos não presentes é " + percentual + "%. Esse valor é referente a " + total +
+          " de alunos.";
+    
+}
 
-// function Media(teorica,pratica){
-//     let total = 10;
+ function Media(teorica,pratica){
+     let calcularMedia = (((2 * teorica) + pratica) / 3).toFixed(1);
 
-//     let calcularMedia = ((2 * teorica) + pratica) / total;
+     return document.getElementById("tela").innerHTML =
+     "<h3>A média do aluno é " + calcularMedia + " .</h3>"
+}
 
-//     return console.log("A média do alino é " + calcularMedia + " .")
-// //    return alert("A média do alino é " + calcularMedia + " .")
+ let manha = 0;
+ let tarde = 0;
+ let noite = 0; 
+ function estacionamento(horario){
+     let hora = horario.slice(0,2);
+     if(hora > 4 && hora <= 11){
+         manha += 1;
+         return;
+     }
+     if(hora > 11 && hora <= 17){
+        tarde += 1;
+        return;
+    }
+     if((hora > 17 && hora <= 23) || (hora >= 00 && hora <= 4)){
+        noite += 1;
+         return;
+     }
+}
 
-// }
+estacionamento("00:30");
+estacionamento("19:30");
+estacionamento("18:30");
+estacionamento("14:30");
+estacionamento("08:30");
+estacionamento("20:30");
+estacionamento("22:30");
+estacionamento("07:30");
+estacionamento("03:30");
+estacionamento("04:30");
 
-// Media(7,8)
+document.write("Acessos da manha: " + manha + "<br>");
+document.write("Acessos da tarde: " + tarde + "<br>");
+document.write("Acessos da noite: " + noite + "<br>");
 
